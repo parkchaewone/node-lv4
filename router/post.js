849b6befe6b85c8417c519831f372a2554eb589e
parkchaewon <xkdldjaos20@gmail.com>
2023-07-12 04:06:55 +0900

    lv4수정

--- router/post.js
+++ router/post.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { Op } = require("sequelize");
 
 const authMiddleware = require("../middlewares/authMiddleware.js");
-const { Posts } = require("../models");
+const { Posts, Users } = require("../models");
 
 // 게시글 생성
 router.post("/posts", authMiddleware, async (req, res) => {
@@ -21,12 +21,23 @@ router.post("/posts", authMiddleware, async (req, res) => {
 // 게시글 목록 조회
 
 router.get("/posts", async (req, res) => {
-  const posts = await Posts.findAll({
-    attributes: ["UserId", "postId", "title", "createdAt", "updatedAt"],
-    order: [["createdAt", "DESC"]],
-  });
-
-  return res.status(200).json({ data: posts });
+  try {
+    const posts = await Posts.findAll({
+      include: { model: Users, attributes: ["email"] },
+      attributes: ["UserId", "postId", "title", "createdAt", "updatedAt"],
+      order: [["createdAt", "DESC"]],
+    });
+    if (!posts.length) {
+      return res.status(400).json({
+        errorMessage: "존재하지 않는 게시글입니다.",
+      });
+    }
+    res.status(200).json({ data: posts });
+  } catch (error) {
+    res.status(400).json({
+      errorMessage: "게시물 조회에 실패 하였습니다.",
+    });
+  }
 });
 
 // 게시글 상세 조회
@@ -76,4 +87,5 @@ router.delete("/:postId", authMiddleware, async (req, res) => {
     }
   }
 });
+
 module.exports = router;
